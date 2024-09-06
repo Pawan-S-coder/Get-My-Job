903720dee8bdb930a54f7891cac3b5f31689546b
Pawan Sharma <pawankumardikshit@gmail.com>
2024-09-06 20:33:31 +0530

    adding PORT

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
  import app from "./index.js";
- app.listen(3000,()=>{
-    console.log('server is listening at 3000');
+
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT,()=>{
+    console.log(`server is Running on Port ${PORT}`);
 
 })
